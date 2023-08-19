f024451170c15e127fa4292f6bc249e1a256db35
andres lopez <zeroxandres@gmail.com>
2023-08-19 18:26:04 -0500

    backend_petclub v2.9.2

--- controllers/image.js
+++ controllers/image.js
@@ -63,7 +63,7 @@ const imagesPost = async (req, res = response) => {
         resource_type: 'video',
         chunk_size: 6000000,
         eager: [
-          { format: 'jpg', transformation: [{ width: 1000, height: 240.99, crop: 'scale'}] }
+          { format: 'jpg', transformation: [{ height: 240.99, crop: 'fit'}] }
         ]
       });
 
