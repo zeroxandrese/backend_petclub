31c4123ce2be6a4cf5f94f226a45931449ee23f8
andres lopez <zeroxandres@gmail.com>
2023-11-11 09:56:27 -0500

    backend petclub v4.5

--- controllers/recoveryPassword.js
+++ controllers/recoveryPassword.js
@@ -82,6 +82,7 @@ const recoveryPasswordPostValidation = async (req, res = response) => {
         await resend.emails.send({
             from: "petClub <admin@petclub.com.pe>",
             to: `${email}`,
+            reply_to: "petclubprueba@gmail.com",
             subject: "Recupera tu clave de acceso en petClub",
             html: `    <html>
             <head>
