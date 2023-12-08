92af92ae8660b72a1a959c211953b14a53bb33f3
andres lopez <zeroxandres@gmail.com>
2023-12-08 17:23:00 -0500

    backend_petclub v5.6.8.9

--- controllers/socket.js
+++ controllers/socket.js
@@ -37,6 +37,7 @@ const socketController = async (socket = new Socket()) => {
         await notifications.save();
 
         socket.to(userValidation.user).emit('mensaje-privado', { de: usuario.nombre });
+        socket.emit('mensaje-prueba', { de: usuario.nombre, uid: usuario._id  });
         console.log('final del proceso');
 
       } catch (error) {
