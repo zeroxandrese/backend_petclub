d08ab6bddd883848d62f0dc639031b046a262687
andres lopez <zeroxandres@gmail.com>
2023-12-09 17:48:43 -0500

    backend_petclub v5.6.9.9

--- controllers/socket.js
+++ controllers/socket.js
@@ -23,8 +23,9 @@ const socketController = async (socket = new Socket()) => {
   await userConnect.save();
   socket.join(usuario._id.toString());
   console.log('_id del usuario luego del join pero el typo',typeof(usuario._id.toString()));
-  console.log(socket.rooms[1]);
-  console.log(typeof(socket.rooms[1]));
+  console.log(Array.from(socket.rooms));
+  console.log(Array.from(socket.rooms[1]));
+  console.log(typeof(Array.from(socket.rooms[1])));
 
   socket.on('notifications-comments', async ({ imgUid }) => {
     if (imgUid) {
